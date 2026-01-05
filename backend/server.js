48532845ddbb7e7e79ed6c852d59a0e6588fbdaf
Sandeep Kumar <sandeepsunny0078@gmail.com>
2026-01-05 21:25:48 +0530

    fix: render deployment (port + api url)

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
+      "https://project-cleanops-frontend.onrender.com",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
