ac301ca7818738c0c55d0782a4ca47a54f0ca10d
Sandeep Kumar <sandeepsunny0078@gmail.com>
2026-01-05 20:53:05 +0530

    fix: render deployment (port + api url)

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,24 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api",
+//   withCredentials: true,
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default api;
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
